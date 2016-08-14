781b3d7c5e612ae0a99320e71baa99e994368368
jaroslavtyc <mail@jaroslavtyc.com>
2016-08-14 21:30:17 +0200

    Removed add and sub from interface because was stupid to force factory methods with given interface without known constructor

--- Granam/Number/NumberInterface.php
+++ Granam/Number/NumberInterface.php
@@ -9,16 +9,4 @@ interface NumberInterface extends ScalarInterface
      * @return int|float
      */
     public function getValue();
-
-    /**
-     * @param int|float|NumberInterface $value
-     * @return NumberInterface
-     */
-    public function add($value);
-
-    /**
-     * @param int|float|NumberInterface $value
-     * @return NumberInterface
-     */
-    public function sub($value);
 }
